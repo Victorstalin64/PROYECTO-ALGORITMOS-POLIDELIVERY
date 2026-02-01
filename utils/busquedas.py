e1de518646e38e1fa8e1af44b312a76dc27636bf
Yourjavii <jaimequispecajiao@gmail.com>
2026-02-01 13:21:56 -0500

    Delete utils directory

--- utils/busquedas.py
+++
@@ -1,9 +0,0 @@
-def busqueda_lineal(lista, nombre):
-    
-    #Búsqueda lineal de un centro por nombre
-    #Retorna el diccionario del centro si lo encuentra, de lo contrario None
-    
-    for elemento in lista:
-        if elemento["nombre"].lower() == nombre.lower():
-            return elemento
-    return None
